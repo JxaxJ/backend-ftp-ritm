a98d645ce9f973d906bc861e9d54f3fdfe354cdf
[JxaxJ] <[klekklaks@gmail.com]>
2023-03-04 00:20:04 +0300

    We need to add dependencies

--- main.py
+++ main.py
@@ -6,22 +6,39 @@ import os
 ftp = FTP(key.FTP)
 ftp.login(key.Login, key.password)
 
-Temporary_array = []
-files_and_folder = []
-files = ftp.nlst()
+def get_file_list(path='/adm'):
 
-for i in range(len(files)):
-    if len(files[i].split('.')) > 1:
-        Temporary_array.append(files[i])
+    f_a_f = []
+    dirs = []
+    f_a_f.extend(ftp.nlst(path))
+    print('faf_1', f_a_f)
 
-    else:
-        ftp.cwd(files[i]) #!!!!!Пытается получить доступ к файлам
-        Temporary_array.append(ftp.nlst())
-        ftp.cwd('../')
+    for i in range(len(f_a_f)):
+        if len(f_a_f[i].split('.')) < 2:
+            dirs.append(f_a_f[i])
+            print('dirs_1', dirs)
 
-for i in range(len(Temporary_array)):
-    for j in range(len(Temporary_array[i])):
-        files_and_folder.append(Temporary_array[i][j])
 
-print(files_and_folder)
+    print('faf_2', f_a_f)
+
+get_file_list()
+
+def get_path_file_to_add_widget():
+    Temporary_array = []
+    files_and_folder = []
+    files = ftp.nlst()
+
+    for i in range(len(files)):
+        if len(files[i].split('.')) > 1:
+            current_directory = ftp.pwd()
+            Temporary_array.append(f"[{current_directory}]/{files[i]}")
+
+        else:
+            ftp.cwd(files[i]) #!!!!!Пытается получить доступ к файлам
+            current_directory = ftp.pwd()
+            Temporary_array.append(f"/{files[i]}")
+            ftp.cwd('../')
+
+    print(Temporary_array)
+
 ftp.quit()
